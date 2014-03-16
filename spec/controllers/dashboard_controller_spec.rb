426e910b67cbf9ba9c14f8e8561aae27cc2654a9
mzp <mzpppp@gmail.com>
2014-03-16 11:12:38 +0000

    add spec for dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,10 +3,23 @@ require 'spec_helper'
 describe DashboardController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    before do
+      @foo = create(:revision, hash_code: 'foo')
+      @bar = create(:revision, hash_code: 'bar')
+      @baz = create(:revision, hash_code: 'baz')
       get 'index'
-      response.should be_success
+    end
+
+    describe 'response' do
+      subject { response }
+      it { should be_success }
+    end
+
+    describe 'revisions' do
+      subject { assigns(:revisions) }
+
+      it { should_not be_empty }
+      it { should include(@foo,@bar,@baz) }
     end
   end
-
 end
